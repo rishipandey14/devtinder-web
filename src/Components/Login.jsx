6711b6bf6de45a3c527090710f72d05f90027f34
Rishi <hopesalive1403@gmail.com>
2025-04-22 15:57:02 +0530

    debugging

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -20,7 +20,6 @@ const Login = () => {
       },{
         withCredentials : true
       });
-      console.log(res.data);
       dispatch(addUser(res.data));
       return navigate("/feed");
       
