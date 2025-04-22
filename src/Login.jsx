946e4fce69645540408a9056e78b98b13184d54e
Rishi <hopesalive1403@gmail.com>
2025-04-22 14:36:26 +0530

    Created redux Store and configured it with userSlice (#11)

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,9 +1,75 @@
-import React from 'react'
+import axios from "axios";
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { addUser } from "./utils/userSlice";
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
 
-export default Login
+  const [emailId, setEmailId] = useState("rishi@gmail.com");
+  const [password, setPassword] = useState("Rishi@1403");
+  const dispatch = useDispatch();
+
+  const handleLogin = async ()  => {
+    try {
+      const res = await axios.post("http://localhost:7000/login", {
+        emailId,
+        password,
+      },{
+        withCredentials : true
+      });
+      console.log(res.data);
+      dispatch(addUser(res.data));
+      
+    } catch (err) {
+      console.log("Error - ", err);
+    }
+  }
+
+
+  return (
+    <div className="flex justify-center">
+      <div className="card bg-base-100 w-96 shadow-sm">
+        <div className="card-body bg-base-200 my-20  border border-gray-300 shadow-lg p-6 rounded-lg w-96 ">
+          <h2 className="card-title justify-center text-2xl">Login</h2>
+
+          <div>
+            <label className="form-control w-full max-w-xs my-2">
+              <div className="label">
+                <span className="label-text">Email: </span>
+              </div>
+              <input
+                id="email"
+                name="email"
+                type="email"
+                className="input input-bordered w-full mb-4"
+                placeholder="Enter Email"
+                value={emailId}
+                onChange={(e) => setEmailId(e.target.value)}
+              />
+            </label>
+            
+            <label className="form-control w-full max-w-xs my-2" >
+              <div className="label">
+                <span className="label-text">Password:</span>
+              </div>
+              <input
+                id="password"
+                name="password"
+                type="password"
+                className="input input-bordered w-full mb-6"
+                placeholder="Enter Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </label>
+          </div>
+          <div className="card-actions justify-center">
+            <button className="btn btn-primary hover:bg-purple-700" onClick={handleLogin }>Login</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
