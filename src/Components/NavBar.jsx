eec9439636477212634400166c82dfe38e4460ce
Rishi <hopesalive1403@gmail.com>
2025-04-22 15:57:38 +0530

    Navbar should changes as user's log in and log out

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,30 +1,36 @@
 import React from 'react'
+import { useSelector } from 'react-redux';
 
 const NavBar = () => {
+  const user = useSelector((store) => store.user);
+
+
   return (
     <div className="navbar bg-base-300 shadow-sm">
-        <div className="flex-1">
-          <a className="btn btn-ghost text-xl">Devtinder</a>
-        </div>
-        <div className="flex gap-2">
-          <div className="dropdown dropdown-end">
-            <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar mx-3">
-              <div className="w-10 rounded-full">
-                <img
-                  alt="Tailwind CSS Navbar component"
-                  src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
-              </div>
+      <div className="flex-1">
+        <a className="btn btn-ghost text-xl">Devtinder</a>
+      </div>
+      <div className="flex gap-2">
+        <div className="dropdown dropdown-end">
+          {user && <div tabIndex={0} role="button" className="btn btn-ghost avatar mx-3 flex items-center"> 
+            <p className='px-4'>Welcome, {user.firstName}</p>
+            <div className="w-10 rounded-full">
+              <img
+                alt="Tailwind CSS Navbar component"
+                src={user.photoUrl} />
             </div>
-            <ul
-              tabIndex={0}
-              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
-              <li><a>Profile</a></li>
-              <li><a>Settings</a></li>
-              <li><a>Logout</a></li>
-            </ul>
           </div>
+          }
+          <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+            <li><a>Profile</a></li>
+            <li><a>Settings</a></li>
+            <li><a>Logout</a></li>
+          </ul>
         </div>
       </div>
+    </div>
   )
 }
 
