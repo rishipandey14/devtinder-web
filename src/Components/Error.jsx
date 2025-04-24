2cb199b84ab8f704de9f974e86e15dbae52bf499
Rishi <hopesalive1403@gmail.com>
2025-04-24 15:44:21 +0530

    Created Error Component

---
+++ src/Components/Error.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Error = () => {
+  return (
+    <div className="text-center text-red-500 font-semibold text-lg mt-10">
+      Something Went Wrong
+    </div>
+  );
+};
+
+export default Error;
